0c8fe250708e23a7273259bfdeef946c585ea99b
lijiewenn <lijiewen2015@163.com>
2018-08-28 16:02:32 +0800

    添加配置文件

---
+++ jiewendb/webapi/src/main/java/com/leekitman/jiewendb/webapi/WebApiConfiguration.java
@@ -0,0 +1,10 @@
+package com.leekitman.jiewendb.webapi;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author LeeKITMAN
+ */
+@Configuration
+public class WebApiConfiguration {
+}
